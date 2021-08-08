d05c3bfcef9d84ac3c6443631c94e2302a3a043d
Jeferson Mesquita <je_2742@hotmail.com>
2021-08-08 18:43:18 -0300

    Update -- Aula 7 --.sql
    ... Aula 7

--- -- Aula 7 --.sql
+++ -- Aula 7 --.sql
@@ -5,3 +5,46 @@
 SELECT coluna1, coluna2
 FROM tabela 
 ORDER BY asc/desc
+
+-- Exemplos --
+SELECT *
+FROM person.Person
+ORDER BY FirstName asc;
+
+SELECT *
+FROM person.Person
+ORDER BY FirstName desc;
+
+SELECT FirstName, LastName
+FROM person.Person
+ORDER BY FirstName asc, LastName desc;
+
+SELECT FirstName, LastName
+FROM person.Person
+ORDER BY MiddleName asc;
+
+-- Desafios -- 
+
+/* 1º Desafio
+Obter o ProductId doos 10 produtos mais caros cadastrados no sistema, listando do mais caro para o mais barato
+-- Dicas --
+- Você terá que usar a tabela Production.product;
+- Você terá que usar o ORDER BY e TOP;
+- E para ordenar você terá que usar o ORDER BY ASC ou DESC dependendo do resultado que está buscando;
+*/
+
+SELECT TOP 10 ProductID
+FROM Production.Product
+ORDER BY ListPrice desc;
+
+/* 2º Desafio
+Obter o nome e número do produto dos produtos que tem o ProductID entre 1~4
+--Dicas --
+- Você terá que usar a tabela Production.product;
+- Você terá que usar o ORDER BY e TOP;
+- E para ordenar você terá que usar o ORDER BY ASC ou DESC dependendo do resultado que está buscando;
+*/
+
+SELECT TOP 4 Name, ProductNumber
+FROM Production.Product
+ORDER BY ProductID asc;
