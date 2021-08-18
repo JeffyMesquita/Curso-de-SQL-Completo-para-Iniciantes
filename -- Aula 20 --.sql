e926aa7e81582cfce3ae1069690b65a9d5991108
Jeferson Mesquita <je_2742@hotmail.com>
2021-08-17 23:31:55 -0300

    Update -- Aula 20 --.sql

--- -- Aula 20 --.sql
+++ -- Aula 20 --.sql
@@ -5,3 +5,27 @@
 /* */
 
 -- EXEMPLOS --
+
+SELECT SalesOrderID, DATEPART(MONTH, OrderDate) AS Mês
+FROM Sales.SalesOrderHeader
+
+SELECT SalesOrderID, DATEPART(DAY, OrderDate) AS Mês
+FROM Sales.SalesOrderHeader
+
+SELECT SalesOrderID, DATEPART(YEAR, OrderDate) AS Mês
+FROM Sales.SalesOrderHeader
+
+SELECT AVG(TotalDue) AS Média, DATEPART(MONTH, OrderDate) AS Mês
+FROM Sales.SalesOrderHeader
+GROUP BY DATEPART(MONTH, OrderDate)
+ORDER BY Mês
+
+SELECT AVG(TotalDue) AS Média, DATEPART(YEAR, OrderDate) AS Ano
+FROM Sales.SalesOrderHeader
+GROUP BY DATEPART(YEAR, OrderDate)
+ORDER BY Ano
+
+SELECT AVG(TotalDue) AS Média, DATEPART(DAY, OrderDate) AS Dia
+FROM Sales.SalesOrderHeader
+GROUP BY DATEPART(DAY, OrderDate)
+ORDER BY Dia
