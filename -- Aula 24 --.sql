e16d40ee440d57df6f7008471ebd86b7642365cd
Jeferson Mesquita <je_2742@hotmail.com>
2021-08-20 02:03:42 -0300

    Update Aula 24
    -- ADD novo banco + Aula 24

--- -- Aula 24 --.sql
+++ -- Aula 24 --.sql
@@ -3,3 +3,27 @@
 -- SELF-JOIN
 
 -- EXEMPLOS
+
+SELECT NOME_COLUNA
+FROM TABELA AS A, TABELA B
+WHERE CONDIÇÃO
+
+SELECT A.ContactName, B.ContactName
+FROM Customers AS A, Customers AS B
+WHERE A.Region = B.Region
+
+SELECT A.ContactName, A.Region, B.ContactName, B.Region
+FROM Customers AS A, Customers AS B
+WHERE A.Region = B.Region
+
+-- Eu quero encontrar(nome e data de cointratação) de todos funcionários que foram
+-- no mesmo ano
+
+-- DESAFIO --
+/* 
+Eu quero saber na tabela de detalhe de pedido[Order Details] quais os produtos tem o mesmo percentual de desconto
+*/
+
+SELECT A.ProductID, A.Discount, B.ProductID, B.Discount
+FROM [Order Details] AS A, [Order Details] AS B
+WHERE A.Discount = B.Discount
