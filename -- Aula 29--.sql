90865ca8805315761b26c42efe0b5adf2b818db9
Jeferson Mesquita <je_2742@hotmail.com>
2021-08-25 21:50:28 -0300

    Create -- Aula 29--.sql

---
+++ -- Aula 29--.sql
@@ -0,0 +1,17 @@
+-- Aula 29--
+
+-- UPDATE + DESAFIO
+
+-- EXEMPLOS --
+
+UPDATE nomeTabela
+SET coluna1 = valor1, 
+    coluna2 = valor2
+WHERE condicao
+
+UPDATE Aula
+SET nome = 'Teste'
+
+UPDATE Aula
+SET nome = 'Mudei'
+Where id = 3
